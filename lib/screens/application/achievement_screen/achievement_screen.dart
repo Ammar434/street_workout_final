eba12e97295f18a6a8c138595523bfea8641f2d8
Ammar434 <raoulammar434@gmail.com>
2022-08-03 22:40:41 +0200

    sdfsd

--- lib/screens/application/achievement_screen/achievement_screen.dart
+++ lib/screens/application/achievement_screen/achievement_screen.dart
@@ -1,15 +1,59 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:street_workout_final/models/rewards.dart';
+import 'package:street_workout_final/screens/application/achievement_screen/components/custom_tile_for_achievement.dart';
+import 'package:street_workout_final/services/firestore_methods/rewards_firestore_methods.dart';
+import 'package:street_workout_final/utils/colors.dart';
+import 'package:street_workout_final/widgets/loading_widget.dart';
 
-import '../../../utils/colors.dart';
 import '../../../utils/constants.dart';
-import 'components/achievement_card.dart';
 
-class AchievementScreen extends StatelessWidget {
+const double categoryHeight = 30;
+const double rewardsTileHeight = 80;
+
+class AchievementScreen extends StatefulWidget {
   const AchievementScreen({Key? key}) : super(key: key);
   static String name = "AchievementScreen";
+
+  @override
+  State<AchievementScreen> createState() => _AchievementScreenState();
+}
+
+class _AchievementScreenState extends State<AchievementScreen> with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  late ScrollController scrollController;
+  late List<RewardsCategory> listRewardCategory;
+  double offsetFrom = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    scrollController.addListener(onScrollListener);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    scrollController.removeListener(onScrollListener);
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void onScrollListener() {
+    
+    for (int i = 0; i < listRewardCategory.length; i++) {
+      final category = listRewardCategory[i];
+    debugPrint(scrollController.offset.toString());
+
+      if (scrollController.offset >= category.offsetFrom && scrollController.offset <= category.offsetTo && tabController.index != i) {
+        tabController.animateTo(i);
+        debugPrint(i.toString());
+
+        debugPrint("hhhhhhhhhhhhhhhhhhh");
+        break;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,62 +73,92 @@ class AchievementScreen extends StatelessWidget {
           ),
           title: const Text("Achievements"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(kPaddingValue),
-          child: DefaultTabController(
-            length: 4,
-            child: Column(
-              children: [
-                TabBar(
-                  indicatorColor: primaryColor,
-                  indicator: BoxDecoration(
-                    color: primaryColor,
-                    borderRadius: BorderRadius.circular(
-                      kRadiusValue,
+        body: FutureBuilder(
+          future: RewardsFirestoreMethods().getRewardsSnapshot(),
+          builder: (context, AsyncSnapshot<List<RewardsCategory>> list) {
+            if (list.connectionState == ConnectionState.waiting) {
+              return const Center(child: LoadingWidget());
+            }
+            listRewardCategory = list.data!;
+
+            tabController = TabController(
+              length: list.data!.length,
+              vsync: this,
+            );
+
+            return Padding(
+              padding: const EdgeInsets.all(kPaddingValue),
+              child: Column(
+                children: [
+                  TabBar(
+                    controller: tabController,
+                    indicatorColor: primaryColor,
+                    indicator: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(
+                        kRadiusValue,
+                      ),
+                    ),
+                    labelColor: Colors.white,
+                    labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+                    onTap: (int index) {
+                      scrollController.animateTo(
+                        list.data!.elementAt(index).offsetFrom,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.bounceOut,
+                      );
+                    },
+                    tabs: List.generate(
+                      list.data!.length,
+                      (index) => Tab(
+                        text: list.data![index].name,
+                      ),
                     ),
                   ),
-                  labelColor: Colors.white,
-                  // unselectedLabelColor: Colors.black,
-                  tabs: const [
-                    Tab(text: "Strength"),
-                    Tab(text: 'Social'),
-                    Tab(text: 'Mental'),
-                    Tab(text: 'Others'),
-                  ],
-                ),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      Column(
-                        children: [
-                          AchievementCard(
-                            title: "Push Strength",
-                            progressionLvl: Random().nextDouble(),
-                          ),
-                          AchievementCard(
-                            title: "Push Strength",
-                            progressionLvl: Random().nextDouble(),
-                          ),
-                        ],
-                      ),
-                      AchievementCard(
-                        title: "Push Strength",
-                        progressionLvl: Random().nextDouble(),
-                      ),
-                      AchievementCard(
-                        title: "Push Strength",
-                        progressionLvl: Random().nextDouble(),
-                      ),
-                      AchievementCard(
-                        title: "Push Strength",
-                        progressionLvl: Random().nextDouble(),
-                      ),
-                    ],
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: list.data!.length,
+                      controller: scrollController,
+                      itemBuilder: (context, index) {
+                        // if (scrollController.offset >= listRewardsCategory.data![index].offsetFrom &&
+                        //     scrollController.offset <= listRewardsCategory.data![index].offsetTo &&
+                        //     tabController.index != index) {
+                        //   debugPrint("hkjhbkjn");
+                        //   tabController.animateTo(index);
+                        // }
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                height: categoryHeight,
+                                child: Text(
+                                  list.data![index].name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: kDefaultTitleSize * 0.75,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            ...List.generate(
+                              list.data![index].rewards.length,
+                              (index2) => CustomTileForAchievement(
+                                rewards: list.data![index].rewards[index2],
+                                height: rewardsTileHeight,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
