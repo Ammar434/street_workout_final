df16cec2cb63514386023976534f7ba40eeca0eb
raoul <raoulammar434@gmail.com>
2023-02-08 01:45:19 +0100

    app name modif

--- lib/screens/application/body.dart
+++ lib/screens/application/body.dart
@@ -43,7 +43,7 @@ class _BodyState extends State<Body> with SingleTickerProviderStateMixin {
       appBar: (index != 2 && index != 1)
           ? AppBar(
               title: Text(
-                "Fit Fighter",
+                "Fitness Fighter",
                 style: Theme.of(context).textTheme.titleMedium,
               ),
               elevation: 0.0,
